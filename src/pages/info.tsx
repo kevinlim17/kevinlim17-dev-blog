6a6f352cfc6c77f8919b707635ba75e94d84b77c
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2023-03-19 22:22:40 +0900

    Revert "Use various ways for styling components by emotion"
    This reverts commit add98f9649354e29658b18318d0c801556343133.

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,7 +1,5 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
-import { Global, css } from '@emotion/react'
-import styled from '@emotion/styled'
 
 type InfoPageProps = {
   data: {
@@ -15,42 +13,6 @@ type InfoPageProps = {
   }
 }
 
-const globalStyle = css`
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-
-    font-size: 20px;
-  }
-`
-
-const TextStyle = css`
-  font-size: 18px;
-  font-weight: 700;
-  color: gray;
-`
-
-/**
- * Tagged Template Literal 방식을 통한 Styled-Component 생성
- * 하이픈('-')을 통해 단어를 연결하는 Kebab Case 사용
- */
-const Text1 = styled.div<{ disable: boolean }>`
-  font-size: 20px;
-  font-weight: 700;
-  text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
-`
-
-/**
- * 객체를 통한 Styled-Component 생성
- * Camel Case를 사용, 스타일 값은 무조건 String Type으로 전달
- */
-const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
-  fontSize: '15px',
-  color: 'blue',
-  textDecoration: disable ? 'line-through' : 'none',
-}))
-
 const InfoPage: FunctionComponent<InfoPageProps> = function ({
   data: {
     site: {
@@ -60,10 +22,7 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      <Global styles={globalStyle} />
-      <div css={TextStyle}>{title}</div>
-      <Text1 disable={true}> {description} </Text1>
-      <Text2 disable={true}> {author} </Text2>
+      {title} {description} {author}
     </div>
   )
 }

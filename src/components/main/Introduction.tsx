1a054601c08c1032fcd334710a8c6509417114bf
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2023-03-23 23:37:08 +0900

    Impl basic frame for main page

---
+++ src/components/main/Introduction.tsx
@@ -0,0 +1,53 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+import ProfileImage from './ProfileImage'
+
+const Background = styled.div`
+  width: 100%;
+  background: linear-gradient(
+    135deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(2, 131, 28, 1) 70%,
+    rgba(0, 255, 109, 1) 100%
+  );
+  color: #ffffff;
+`
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  width: 768px;
+  height: 400px;
+  margin: 0 auto;
+`
+
+const SubTitle = styled.div`
+  font-size: 20px;
+  font-weight: 400;
+`
+
+const Title = styled.div`
+  margin-top: 5px;
+  font-size: 35px;
+  font-weight: 700;
+`
+
+const Introduction: FunctionComponent = function () {
+  return (
+    <Background>
+      <Wrapper>
+        <ProfileImage />
+
+        <div>
+          <SubTitle>
+            Do Things For Better Community, Writing Codes & Developing Stories.
+          </SubTitle>
+          <Title>kevinlim17, Android Native Developer.</Title>
+        </div>
+      </Wrapper>
+    </Background>
+  )
+}
+
+export default Introduction
---
+++ src/components/main/ProfileImage.tsx
@@ -0,0 +1,18 @@
+import React, { FunctionComponent } from 'react'
+import styled from '@emotion/styled'
+
+const PROFILE_IMAGE_LINK =
+  'https://avatars.githubusercontent.com/u/86971052?s=400&u=71459e5c8a82fa3440c7c7f4d254d58afc204959&v=4'
+
+const ProfileImageWrapper = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 30px;
+  border-radius: 30%;
+`
+
+const ProfileImage: FunctionComponent = function () {
+  return <ProfileImageWrapper src={PROFILE_IMAGE_LINK} alt="Profile Image" />
+}
+
+export default ProfileImage

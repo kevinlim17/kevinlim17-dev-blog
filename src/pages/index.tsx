0082f8fa8383281dd68a174c1dd00ce62be7bd4f
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2023-03-28 00:27:07 +0900

    Ensure linking post items with slug data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -104,6 +104,9 @@ export const getPostList = graphql`
       edges {
         node {
           id
+          fields {
+            slug
+          }
           frontmatter {
             title
             summary

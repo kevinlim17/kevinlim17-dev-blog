2ced5ac273cb4370cce1b17e20ba7f9323031604
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2023-03-27 22:39:33 +0900

    Add Slug field to markdown data

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,11 +4,12 @@
  * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-node/
  */
 
-const path = require('path');
+const path = require('path')
+const { createFilePath } = require(`gatsby-source-filesystem`)
 
 //Setup Import Alias
 exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
-  const output = getConfig().output || {};
+  const output = getConfig().output || {}
 
   actions.setWebpackConfig({
     output,
@@ -16,8 +17,19 @@ exports.onCreateWebpackConfig = ({ getConfig, actions }) => {
       alias: {
         components: path.resolve(__dirname, 'src/components'),
         utils: path.resolve(__dirname, 'src/utils'),
-        hooks: path.resolve(__dirname, 'src/hooks')
+        hooks: path.resolve(__dirname, 'src/hooks'),
       },
     },
-  });
-};
+  })
+}
+
+// Generate a Slug Each Post Data
+exports.onCreateNode = ({ node, getNode, actions }) => {
+  const { createNodeField } = actions
+
+  if (node.internal.type === `MarkdownRemark`) {
+    const slug = createFilePath({ node, getNode })
+
+    createNodeField({ node, name: 'slug', value: slug })
+  }
+}

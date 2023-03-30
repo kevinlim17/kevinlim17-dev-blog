d07b0f02561c704fd7b2771f68e3160ec7f4f364
Seung Hyeon Lim <kevinlsh17@khu.ac.kr>
2023-03-31 00:47:07 +0900

    Add PostContent Component to post page

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 import { PostPageItemType } from 'types/PostItem.types'
 import Template from 'components/common/Template'
 import PostHead from '../components/post/PostHead'
+import PostContent from 'components/post/PostContent'
 
 type PostTemplateProps = {
   data: {
@@ -40,6 +41,7 @@ const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
         categories={categories}
         thumbnail={gatsbyImageData}
       />
+      <PostContent html={html} />
     </Template>
   )
 }
